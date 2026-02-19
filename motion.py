277b5f7af0a8f678de9dc1159f52be2458ca7e2c
mimi6612 <72131411+mimi6612@users.noreply.github.com>
2026-02-19 21:29:40 +0900

    Update motion.py

--- motion.py
+++ motion.py
@@ -4,3 +4,8 @@ def angle(t):
     A = math.radians(30)
     omega = 2 * math.pi
     return A * math.sin(omega * t)
+
+def angle2(2)
+    A = math.radians(30)
+    omega = 2 * math.pi
+    return A * math.sin(omega * t + math.pi)
