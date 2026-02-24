344131d18e4b9c3acd4a56dd748c12dac13be83d
mimi6612 <mimimimi6612@gmail.com>
2026-02-24 18:20:17 +0900

    phaseを指定できるようにしました。

--- motion.py
+++ motion.py
@@ -5,7 +5,14 @@ def angle(t):
     omega = 2 * math.pi
     return A * math.sin(omega * t)
 
-def angle2(2)
+def angle2(t):
     A = math.radians(30)
     omega = 2 * math.pi
     return A * math.sin(omega * t + math.pi)
+
+def angle3(t, phase):
+    A = math.radians(30)
+    omega = 2 * math.pi
+    return A * math.sin(omega * t + phase)
+
+
