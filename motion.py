47c1f0892ed9f23c6a3b94e379acb78472bae1a7
mimi6612 <mimimimi6612@gmail.com>
2026-02-25 20:08:02 +0900

    周波数を揺らしたangleを計算する関数を追加

--- motion.py
+++ motion.py
@@ -15,4 +15,14 @@ def angle3(t, phase):
     omega = 2 * math.pi
     return A * math.sin(omega * t + phase)
 
+# 周波数を揺らす
+def omega(t):
+    base_omega = 2 * math.pi
+    fluctuation = 0.5 * math.sin(0.5 * t)  # 周波数の揺らぎ
+    return base_omega + fluctuation
+
+def angle4(t):
+    A = math.radians(30)
+    return A * math.sin(omega(t) * t)
+
 
